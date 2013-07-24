69df68f8d82bc2ae73581e777f759c7f17e142ca
Stephen Zweibel <srzweibel@gmail.com>
2013-07-24 15:48:59 -0400

    added nts

--- guteparsed.py
+++ guteparsed.py
@@ -4,7 +4,7 @@ import pprint
 
 
 g = Graph()
-g.parse("serialized_agents_5.nt", format="nt")
+g.parse("serialized_agents.nt", format="nt")
 
 for j in g.triples((None,URIRef('http://www.gutenberg.org/2009/pgterms/name'),None)):
     print(j)
